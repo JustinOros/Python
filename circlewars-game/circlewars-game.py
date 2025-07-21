3569411215340bcb0112fc7c951817f285e70e08
Justin Oros <JustinOros@gmail.com>
2025-07-21 16:06:07 -0700

    Update circlewars-game.py

--- circlewars-game/circlewars-game.py
+++ circlewars-game/circlewars-game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Description: circlewars - A simple game written in Python.
 # Usage: python3 circlewars-game.py
-# Author: Justin Oros (improved by ChatGPT)
+# Author: Justin Oros
 # Source: https://github.com/JustinOros
 
 import pygame
