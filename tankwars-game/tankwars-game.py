dda047cc04c04abe809ebba3af3ba715dd20e1d6
JustinOros <JustinOros@gmail.com>
2025-08-25 19:22:55 -0700

    Renamed tank.obj to object-tank.obj

--- tankwars-game/tankwars-game.py
+++ tankwars-game/tankwars-game.py
@@ -78,10 +78,10 @@ tank_model = None
 tank_display_list = None
 TANK_SCALE = 0.003
 
-if os.path.isfile("tank.obj"):
+if os.path.isfile("object-tank.obj"):
     try:
         import pywavefront
-        tank_model = pywavefront.Wavefront("tank.obj", create_materials=True, collect_faces=True, parse=True)
+        tank_model = pywavefront.Wavefront("object-tank.obj", create_materials=True, collect_faces=True, parse=True)
         OBJ_AVAILABLE = True
         tank_display_list = glGenLists(1)
         glNewList(tank_display_list, GL_COMPILE)
